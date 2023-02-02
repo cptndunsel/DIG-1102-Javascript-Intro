58e41663215e3a9f7226ba8a32f4a652a19e0809
unknown <dbboell@yahoo.com>
2023-02-02 15:36:37 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var firstName = "David";
+let lastName = " Boell";
+const age = 25;
+
+alert("My name is " + firstName + lastName) 
+
+alert("I am " + age + " years old");
+
+console.log("Javascript is interesting.")
